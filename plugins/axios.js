c777d578750649a8e7de90da2fa67c68e0eb338f
托儿索 <965671101@qq.com>
2019-09-05 16:05:44 +0800

    请求错误拦截,机票首页和组件布局完成

---
+++ plugins/axios.js
@@ -0,0 +1,12 @@
+import { Message } from "element-ui";
+
+export default ({ $axios }) => {
+    //错误拦截,onError用于错误拦截蓝旗
+    $axios.onError(res => {
+        //解构出错误信息,和代码
+        const { message, statusCode } = res.response.data;
+        if (statusCode === 400) {
+            Message.error(message)
+        }
+    })
+}
