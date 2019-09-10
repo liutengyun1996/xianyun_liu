6121e83b133ee8831eeccbe19958088fa82fb532
托儿索 <965671101@qq.com>
2019-09-10 17:24:35 +0800

    跳转到订单页

--- components/air/flightsItem.vue
+++ components/air/flightsItem.vue
@@ -40,7 +40,8 @@
             </el-col>
             <el-col :span="5" class="price">{{item.org_settle_price}}</el-col>
             <el-col :span="3" class="choose-button">
-              <el-button type="warning" size="mini">选定</el-button>
+              <!-- 座位id -->
+              <el-button type="warning" size="mini" @click="handleChoose(item.seat_xid)">选定</el-button>
               <p>剩余：{{item.discount}}</p>
             </el-col>
           </el-row>
@@ -96,6 +97,18 @@ export default {
       const min = dis % 60;
 
       return `${hours}时${min}分`;
+    }
+  },
+  methods:{
+    //跳转到机票订单页
+    handleChoose(seat_xid){
+      this.$router.push({
+        path:"/air/order",
+        query:{
+          id:this.data.id,
+          seat_xid
+        }
+      })
     }
   }
 };
